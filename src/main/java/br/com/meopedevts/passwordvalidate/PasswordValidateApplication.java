5de958bc96968757756b5b4a88145491e7049505
Marcelo-SM <marcelo.meope@icloud.com>
2023-10-31 17:27:56 -0300

    Created the Password Validate project to address the back-end challenge proposed by Back-end Brasil

---
+++ src/main/java/br/com/meopedevts/passwordvalidate/PasswordValidateApplication.java
@@ -0,0 +1,13 @@
+package br.com.meopedevts.passwordvalidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordValidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordValidateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meopedevts/passwordvalidate/PasswordValidateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meopedevts.passwordvalidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
